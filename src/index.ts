a91a9b9e0fd7049eb67ca636640b34b88510bca4
William Aynsley <willaynsley@gmail.com>
2023-09-27 11:48:15 +0800

    edit index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
+require('dotenv').config();
+
+import { Router } from 'itty-router';
+import productsRouter from './routes/ProductsRouter';
+
+const router = Router();
+
+// Register the products router
+router.all('/api/*', productsRouter.handle);
+
+router.all('*', () => new Response('Catch-all route triggered'));
+
+async function handleRequest(request: Request): Promise<Response> {
+    console.log('Received request:', request.method, request.url);
+    
+    const response = await router.handle(request);
+
+    console.log('Response in index:', response);
+    
+    if (!response) {
+        console.log('Using fallback response');
+        return new Response("Not able to handle this request");
+    }
+    
+    console.log('Router provided a response:', response.status);
+    return response;
+}
+
+// Register the event listener for fetch
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request));
+});
+
+
+
+
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -8,26 +44,27 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-}
+// export interface Env {
+// 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+// 	// MY_KV_NAMESPACE: KVNamespace;
+// 	//
+// 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+// 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+// 	//
+// 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+// 	// MY_BUCKET: R2Bucket;
+// 	//
+// 	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+// 	// MY_SERVICE: Fetcher;
+// }
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+// export default {
+// 	async fetch(
+// 		request: Request,
+// 		env: Env,
+// 		ctx: ExecutionContext
+// 	): Promise<Response> {
+// 		console.log('in fetch');
+// 		return new Response("Hello World!");
+// 	},
+// };
